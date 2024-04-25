ab70574d8393be15132dedc9d6a4408662435914
bingual <bingual76@gmail.com>
2024-04-25 23:43:55 +0900

    include, css 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -30,7 +30,7 @@ class SignupForm(UserCreationForm):
                 "회원가입",
                 css_class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none "
                 "focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 "
-                "dark:hover:bg-blue-700 dark:focus:ring-blue-800",
+                "dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer",
             )
         )
 
@@ -61,7 +61,7 @@ class LoginForm(AuthenticationForm):
                 "로그인",
                 css_class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none "
                 "focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 "
-                "dark:hover:bg-blue-700 dark:focus:ring-blue-800",
+                "dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer",
             )
         )
 
@@ -87,6 +87,6 @@ class ProfileForm(ModelForm):
                 "저장",
                 css_class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none "
                 "focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 "
-                "dark:hover:bg-blue-700 dark:focus:ring-blue-800",
+                "dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer",
             )
         )
