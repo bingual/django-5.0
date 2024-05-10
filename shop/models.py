907f37861b3e115bf63d87c24b5b10dd16b97f90
bingual <bingual76@gmail.com>
2024-05-10 23:51:48 +0900

    django-imagekit 설치, Brand, Category 모델 추가

--- shop/models.py
+++ shop/models.py
@@ -1,34 +1,60 @@
-from os.path import splitext
-
 from django.db import models
-from django_lifecycle import LifecycleModelMixin, BEFORE_SAVE, hook
+from imagekit.models import ProcessedImageField
+from pilkit.processors import ResizeToFill
 
-from theme.utils import make_thumb, uuid_name_upload_to
+from theme.utils import uuid_name_upload_to
 
 
-class Product(LifecycleModelMixin, models.Model):
-    brand = models.CharField(max_length=100)  # TODO: 나중에 Brand Model ForeignKey 변환
-    category = models.CharField(
-        max_length=100
-    )  # TODO: 나중에 Category Model ForeignKey 변환
-    thumb = models.ImageField(upload_to=uuid_name_upload_to)
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Brand(TimeStampedModel):
+    logo_thumb = ProcessedImageField(
+        upload_to=uuid_name_upload_to,
+        processors=[ResizeToFill(82, 82)],
+        format="JPEG",
+        options={"quality": 100},
+    )
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+# class BrandThumbnail(TimeStampedModel):
+#     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+#     thumb = ProcessedImageField(
+#         upload_to=uuid_name_upload_to,
+#         processors=[ResizeToFill(512, 512)],
+#         format="JPEG",
+#         options={"quality": 100},
+#     )
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(TimeStampedModel):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    thumb = ProcessedImageField(
+        upload_to=uuid_name_upload_to,
+        processors=[ResizeToFill(512, 512)],
+        format="JPEG",
+        options={"quality": 100},
+    )
     name = models.CharField(max_length=100, unique=True)
     price = models.IntegerField()
     sale_price = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ["-pk"]
 
     def __str__(self):
-        return f"{self.brand} - {self.brand} - {self.thumb} - {self.name} - {self.price} - {self.sale_price} - {self.created_at}"
-
-    @hook(BEFORE_SAVE, when="thumb", has_changed=True)
-    def on_image_change(self):
-        if self.thumb:
-            image_width = self.thumb.width
-            image_extension = splitext(self.thumb.name)[-1].lower()
-
-            if image_width > 1024 or image_extension not in (".jpg", ".jpeg"):
-                thumb_file = make_thumb(self.thumb.file, 512, 512, 100)
-                self.thumb.save(thumb_file.name, thumb_file, save=False)
+        return self.name
